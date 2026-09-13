07ba1ec8e34ca44ac79b1f30823a7ee89d0259b7
Koyel Basu <koyelbasu240306@gmail.com>
2026-09-13 15:41:51 +0530

    cru

---
+++ day3.CRUD/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let port = 400;
+
+let users = [{
+    name: "Koyel",
+    age: 89
+}];
+
+app.listen(port, () =>{
+    console.log(`Server is running on ${port}`);
+});
+
+app.get('/', (req, res) =>{
+    res.send(users)
+})
+
+app.post('/create', (req, res)=>{
+    let body = req.body;
+
+    users.push(body);
+
+    res.send(users);
+})

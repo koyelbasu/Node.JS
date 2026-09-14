5a3f9d2595a1bad099ace14d2f4f6a013dfa7f71
Koyel Basu <koyelbasu240306@gmail.com>
2026-09-14 10:06:00 +0530

    Add update -> push

--- day3.CRUD/server.js
+++ day3.CRUD/server.js
@@ -11,15 +11,37 @@ let users = [{
     age: 89
 }];
 
-
+//get = read
  app.get('/', (req, res) =>{
     res.send(users)
  })
 
+
+ //post = create
 app.post('/create', (req, res)=>{
     let body = req.body;
 
     users.push(body);
 
+    res.send("user saved successfully");
+})
+
+//put = update
+app.put("/update/:id" , (req, res) =>{
+    let {id} = req.params;
+
+    let updateUser = users.map((val)=> val.id === id? {id, name : "unchanged", age: 90} : val);
+    res.send(updateUser);
+});
+
+//delete = delete
+app.delete("/delete/:id" , (req, res) =>{
+    let {id} = req.params;
+    let userData = users.filter((val) => val.id !== id);
+    users = userData;
     res.send(users);
+});
+
+app.listen(port, ()=>{
+    console.log(`server is running at ${port}`);
 })

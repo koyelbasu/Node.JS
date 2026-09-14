3ab4539e2f7ede9c1bef240ab9f73b58347ab6ec
Koyel Basu <koyelbasu240306@gmail.com>
2026-09-14 20:11:07 +0530

    Add new API

--- day2.expressjs/server.js
+++ day2.expressjs/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("hey you reachedd here");
 });
@@ -16,8 +17,19 @@ app.get("/products", (req, res) =>{
     res.send("ok post it")
    });
 
+let user = [{
+    id:1,
+    name: "Koyel",
+    age: 20
+}];
 
-let port = 500;
+app.get('/users', (req, res) =>{
+    res.send(` show the result ${user}`)
+})
+
+
+
+let port = 4000;
 
 app.listen(port, ()=>{
     console.log(`Server is running on ${port}`)
